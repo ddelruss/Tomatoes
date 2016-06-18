e220ac8a2e214b03a9e56d7c0ffe1773ea8d4777
Damien Del Russo <ddelruss@mac.com>
2016-06-18 15:47:29 -0700

    plumbing works, about to add testing

---
+++ Server/readme.md
@@ -0,0 +1,1 @@
+placeholder readme for npm
--- Server/routes/routes.js
+++ Server/routes/routes.js
@@ -1,9 +1,27 @@
 var appRouter = function(app) {
  
+ 	var storage = require('node-persist')
+	storage.initSync();
+	loadDefaultData();
+	//storage.clear();
+	//storage.setItem('name','Damien');
+	//console.log(storage.getItem('name'));
+	
 	app.get("/", function(req, res) {
-	    res.send("Hello World");
+	    res.send(storage.getItem('default'));
 	});
 	
+	function loadDefaultData() {
+		if (storage.values.size == 0) {
+			var aMovie = {
+				movie_name: "A Movie Name",
+				image_url: "http://ielts-results.weebly.com/uploads/4/0/6/6/40661105/1113084_orig.jpg",
+				rating: "2.5",
+				description: "A compelling movie description updated"
+			}
+			storage.setItem('default', aMovie)
+		}
+	}
 }
  
 module.exports = appRouter;
