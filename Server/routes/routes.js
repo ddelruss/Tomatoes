4cbe36769d6d3c32ff30d3764b837fa46b01d0e8
Damien Del Russo <ddelruss@mac.com>
2016-06-18 23:52:10 -0700

    Get working with all filtering.

--- Server/routes/routes.js
+++ Server/routes/routes.js
@@ -7,19 +7,25 @@ var appRouter = function(app) {
 	//storage.setItem('name','Damien');
 	//console.log(storage.getItem('name'));
 	
-	app.get("/", function(req, res) {
-		console.log("Current db size %s", storage.values().length);
-	    // res.send(storage.getItem('default').movie_name);
-		var value = storage.getItem('default')
-		console.log("Returning:\n%s", value)
-	    res.send(value);
-	});
+	// app.get("/", function(req, res) {
+	// 	console.log("Current db size %s", storage.values().length);
+	//     // res.send(storage.getItem('default').movie_name);
+	// 	var value = storage.getItem('default')
+	// 	console.log("Returning:\n%s", value)
+	//     res.send(value);
+	// });
 
 	app.get("/movies", function(req, res) {
-		console.log("Current db size %s", storage.values().length);
+		// console.log("Current db size %s", storage.values().length);
+		console.log(req.query)
+		// var	values = req.query.movie_name ? storage.valuesWithKeyMatch(req.query.movie_name) : storage.values()
 		var values = storage.values()
-		console.log("Returning:\n%s", values)
-	    res.send(values);
+		var searchTerm = req.query.movie_name
+		function match(movie) {
+			return movie.movie_name.indexOf(searchTerm) > -1
+		}
+		var filterValues = searchTerm ? values.filter(match) : values
+		res.send(filterValues)
 	});
 	
 	function loadDefaultData() {
@@ -28,10 +34,15 @@ var appRouter = function(app) {
 				movie_name: "Default Movie",
 				image_url: "http://ielts-results.weebly.com/uploads/4/0/6/6/40661105/1113084_orig.jpg",
 				rating: "2.5",
-				description: "A compelling movie description updated"
+				description: "A compelling movie description"
 			}
-			storage.setItem('default', aMovie)
+			storage.setItem(aMovie.movie_name, aMovie)
 		}
+	}
+	
+	function match(movie, search) {
+		console.log("Comparing %s to %s", movie.movie_name, searc)
+		return movie.movie_name.indexOf(search) > -1
 	}
 }
  
--- Server/spec/appSpec.js
+++ Server/spec/appSpec.js
@@ -4,36 +4,88 @@ var base_url = "http://localhost:3000/"
 
 describe("Tomatoes Server Tests", function() {
   describe("GET /", function() {
-    it("returns status code 200", function(done) {
-      request.get(base_url, function(error, response, body) {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-    });
-
+  
 // This test is mostly only relevant before we can create movies
-    it("returns Default Movie", function(done) {
-      request.get(base_url, function(error, response, body) {
-        expect(JSON.parse(body).movie_name).toBe("Default Movie");
-        done();
-      });
-    });
+  //   it("returns Default Movie", function(done) {
+  //     request.get(base_url, function(error, response, body) {
+  //       expect(JSON.parse(body).movie_name).toBe("Default Movie");
+  //       done();
+  //     });
+  //   });
   });
+  var movies_url = base_url + "movies"
   describe("GET /movies", function() {
-	var movies_url = base_url + "movies/"
+
+      // it("returns status code 200", function(done) {
+      //   request.get(base_url, function(error, response, body) {
+      //     expect(response.statusCode).toBe(200);
+      //     done();
+      //   });
+      // });
+
+      it("returns status code 200", function(done) {
+        request.get(movies_url, function(error, response, body) {
+          expect(response.statusCode).toBe(200);
+          done();
+        });
+      });
+
+
 	it("returns list of movies with 1 item", function(done) {
 	  request.get(movies_url, function(error, response, body) {
 	    expect(JSON.parse(body).length).toBe(1);
 	    done();
 	   });
 	 });
+	 
  	it("1st movie in list is the default", function(done) {
  	  request.get(movies_url, function(error, response, body) {
 		  expect(JSON.parse(body)[0].rating).toBe("2.5"); // the default movie
  	    done();
  	   });
  	 });
+
+	 var unmatching_url = movies_url + "?movie_name=UNKNOWN"
+  	it("Unmatching query parameter finds nothing", function(done) {
+  	  request.get(unmatching_url, function(error, response, body) {
+  	    expect(JSON.parse(body).length).toBe(0);
+  	    done();
+  	   });
+  	 });
+
+ 	 var exact_matching_url = movies_url + "?movie_name=Default Movie"
+   	it("Matching query parameter finds match", function(done) {
+   	  request.get(exact_matching_url, function(error, response, body) {
+   	    expect(JSON.parse(body).length).toBe(1);
+   	    done();
+   	   });
+   	 });
+
+  	 var partial_matching_url = movies_url + "?movie_name=Defaul" // substring of Default
+    	it("Matching query parameter finds match", function(done) {
+    	  request.get(partial_matching_url, function(error, response, body) {
+    	    expect(JSON.parse(body).length).toBe(1);
+    	    done();
+    	   });
+    	 });
+	
   });
+
+	//   describe("POST /movies", function() {
+	// var movies_url = base_url + "movies/"
+	// it("returns list of movies with 1 item", function(done) {
+	//   request.get(movies_url, function(error, response, body) {
+	//     expect(JSON.parse(body).length).toBe(1);
+	//     done();
+	//    });
+	//  });
+	//  	it("1st movie in list is the default", function(done) {
+	//  	  request.get(movies_url, function(error, response, body) {
+	// 	  expect(JSON.parse(body)[0].rating).toBe("2.5"); // the default movie
+	//  	    done();
+	//  	   });
+	//  	 });
+	//   });
 
   
 });
